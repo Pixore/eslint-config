d4e5dfa63c6c9d35bc5bd84801679574a9d08a99
Jose Albizures <albizures3601@gmail.com>
2017-06-18 06:02:18 -0600

    feat(react): add jsx and react rules

--- index.js
+++ index.js
@@ -7,7 +7,16 @@ module.exports = {
     'prefer-const': 'error',
     camelcase: 0,
     'no-alert': 'error',
-    'no-console': ['error', {allow: ['clear']}]
+    'no-console': ['error', {allow: ['clear']}],
+    'react/prop-types': 'error',
+    'react/require-default-props': 'error',
+    'react/jsx-max-props-per-line': ['error', {
+      maximum: 3
+    }],
+    'react/jsx-no-duplicate-props': 'error',
+    'react/jsx-wrap-multilines': 'error',
+    'react/no-unknown-property': 'error',
+    'react/no-unused-prop-types': 'error'
   },
   env: {
     browser: true,
